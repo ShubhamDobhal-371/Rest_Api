866a55d43d6e850927262dfbf179ea16c80b20a5
ShubhamDobhal-371 <shubham.rdobhal@gmail.com>
2024-06-15 16:34:04 +0530

    commit build control for create user.

--- src/main/java/com/shiubhu/RestApi_01/controller/UserController.java
+++ src/main/java/com/shiubhu/RestApi_01/controller/UserController.java
@@ -1,11 +1,29 @@
 package com.shiubhu.RestApi_01.controller;
 
+import com.shiubhu.RestApi_01.model.User;
 import com.shiubhu.RestApi_01.service.UserServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api") //indicate which type of http methode call
 public class UserController {
     @Autowired
     private UserServiceI userServiceI;
+
+
+
+//    @RequestMapping(method = RequestMethod.POST, name = "/createuser")---->(Use before postmapping not exisest)
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+
+        User createduser = userServiceI.createUser(user);
+
+        return new ResponseEntity<User>(createduser, HttpStatus.CREATED);//201
+
+    }
+
+
 }
